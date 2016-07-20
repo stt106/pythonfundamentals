4873b9011cf06bc23182cf8a10fa8fcd5363f8c9
Tiantang Sun <stt106@users.noreply.github.com>
2016-07-20 12:37:19 +0100

    Rename set-demo.py to collection/set-demo.py

---
+++ collection/set-demo.py
@@ -0,0 +1,1 @@
+# set is an unordered collection of unique and immutable objects (everything is an object in python)
--- set-demo.py
+++
@@ -1,1 +0,0 @@
-# set is an unordered collection of unique and immutable objects (everything is an object in python)
